3747c5da36352c97b1aeb04ed95a3905ebfdda0c
Motohiro Yakura <motohiro686@yahoo.co.jp>
2019-01-02 01:51:28 +0900

    ひとまず完成

--- keyboards/miniaxe/keymaps/motoyak/keymap.c
+++ keyboards/miniaxe/keymaps/motoyak/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2018 ENDO Katsuhiro <ka2hiro@curlybracket.co.jp>
+ /* Copyright 2018 ENDO Katsuhiro <ka2hiro@curlybracket.co.jp>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,6 +15,9 @@
  */
 #include QMK_KEYBOARD_H
 
+#define TAPPING_TERM 100
+#define PERMISSIVE_HOLD
+
 // layers
 #define _QWERTY 0
 #define _LOWER 1
@@ -28,13 +31,6 @@ enum custom_keycodes {
   ADJUST,
 };
 
-//enum motoyak_keycodes
-//{
-//    DYNAMIC_MACRO_RANGE = SAFE_RANGE,
-//};
-//
-//#include "dynamic_macro.h"
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
@@ -46,16 +42,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------|           |------+------+------+------+------|
  * |   Z  |   X  |   C  |   V  |   B  |           |   N  |   M  |   ,  |   .  |   /  |
  * `-------------+------+------+------|           |------+------+------+------+------'
- *               |      | MHEN |Esc   |           |Spc   | KANA |      |
- *               | L-Alt| LOWER|L-Ctrl|           |R-Sft | RAISE| R-Gui|
+ *               |      | MHEN | BKSP |           | Spc  | KANA | Esc  |
+ *               | L-Alt| LOWER|L-Ctrl|           | R-Sft| RAISE| R-Gui|
  *               `--------------------'           `--------------------'
  */
 [_QWERTY] = LAYOUT(
-  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
-  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                    KC_LALT, LT(_LOWER, KC_MHEN), MT(MOD_LCTL, KC_ESC),
-                                                    MT(MOD_RSFT, KC_SPC), LT(_RAISE, KC_KANA), KC_RGUI
+  KC_Q,    KC_W,    KC_E,     KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
+  KC_A,    KC_S,    KC_D,     KC_F,    KC_G,         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
+  KC_Z,    KC_X,    KC_C,     KC_V,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
+                    MT(MOD_LALT, KC_ESC), LT(_LOWER, KC_MHEN), MT(MOD_LCTL, KC_BSPC),
+                                                    MT(MOD_RSFT, KC_SPC), LT(_RAISE, KC_KANA), MT(MOD_RGUI, KC_ESC)
 ),
 
 /* Raise
@@ -67,8 +63,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------|           |------+------+------+------+------|
  * |  Ins |   -  |   =  |  \   |  `   |           |   ;  |   '  |   <  |   [  |   ]  |
  * `-------------+------+------+------|           |------+------+------+------+------'
- *               |      | MHEN |Esc   |           |Spc   | KANA |      |
- *               | L-Alt| LOWER|L-Ctrl|           |R-Sft | RAISE| R-Gui|
+ *               |      | MHEN | BKSP |           | Spc  | KANA | Esc  |
+ *               | L-Alt| LOWER|L-Ctrl|           | R-Sft| RAISE| R-Gui|
  *               `--------------------'           `--------------------'
  */
 [_RAISE] = LAYOUT(
@@ -83,18 +79,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,----------------------------------.           ,----------------------------------.
  * |   1  |   2  |   3  |   4  |   5  |           |   6  |   7  |   8  |   9  |   0  |
  * |------+------+------+------+------|           |------+------+------+------+------|
- * |  Tab |   R  |   E  |   I  | PSc  |           | Left | Down |  Up  | Right| Enter|
+ * |  Tab |   R  |   E  |   I  | SRq  |           | Left | Down |  Up  | Right| Enter|
  * |------+------+------+------+------|           |------+------+------+------+------|
- * |  Del |   S  |   U  |   B  | SRq  |           | Home | PgDn | PgUp | End  | BkSp |
+ * |  Del |   S  |   U  |   B  | PSc  |           | Home | PgUp | PgUp | End  | BkSp |
  * `-------------+------+------+------|           |------+------+------+------+------'
- *               |      | MHEN |Esc   |           |Spc   | KANA |      |
- *               | L-Alt| LOWER|L-Ctrl|           |R-Sft | RAISE| R-Gui|
+ *               |      | MHEN | BKSP |           | Spc  | KANA | Esc  |
+ *               | L-Alt| LOWER|L-Ctrl|           | R-Sft| RAISE| R-Gui|
  *               `--------------------'           `--------------------'
  */
 [_LOWER] = LAYOUT(
   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-  KC_TAB,  KC_R,    KC_E,    KC_I,    KC_PSCR,     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_ENT,
-  KC_DEL,  KC_S,    KC_U,    KC_B,    KC_SYSREQ,   KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_BSPC,
+  KC_TAB,  KC_R,    KC_E,    KC_I,    KC_SYSREQ,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_ENT,
+  KC_DEL,  KC_S,    KC_U,    KC_B,    KC_PSCR,      KC_HOME, KC_END,  KC_PGUP, KC_PGDN, KC_BSPC,
                     _______, _______, _______,      _______,  _______, _______
 ),
 
@@ -104,18 +100,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,----------------------------------.           ,----------------------------------.
  * |  F1  |  F2  |  F3  |  F4  |  F5  |           |   F6 |  F7  |  F8  |  F9  |  F10 |
  * |------+------+------+------+------|           |------+------+------+------+------|
- * |  F11 |  F12 |      |      |      |           |      |      |      |      |      |
+ * |  Tab |   R  |   E  |   I  | SRq  |           | MPlay| MSTOP| Bri+ | Bri- | Enter|
  * |------+------+------+------+------|           |------+------+------+------+------|
- * | Reset|      |      |      |      |           | Prev | Next | Vol- | Vol+ | Play |
+ * | L-Sft|   S  |   U  |   B  | PSc  |           | Prev | Next | Vol- | Vol+ | Mute |
  * `-------------+------+------+------|           |------+------+------+------+------'
- *               |      | MHEN |Esc   |           |Spc   | KANA |      |
- *               | L-Alt| LOWER|L-Ctrl|           |R-Sft | RAISE| R-Gui|
+ *               |      | MHEN | BKSP |           | Spc  | KANA | Esc  |
+ *               | L-Alt| LOWER|L-Ctrl|           | R-Sft| RAISE| R-Gui|
  *               `--------------------'           `--------------------'
  */
 [_ADJUST] =  LAYOUT(
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
-  KC_F11,  KC_F12,  _______, _______, _______,      _______, _______, _______, _______, _______,
-  RESET,   _______, _______, _______, _______,      KC_MPRV, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY,
+  KC_TAB,  KC_R,    KC_E,    KC_I,    KC_SYSREQ,    KC_MPLY, KC_MSTP, KC_BRIU, KC_BRID, KC_ENT,
+  KC_LSFT, KC_S,    KC_U,    KC_B,    KC_PSCR,      KC_MPRV, KC_MNXT, KC_VOLD, KC_VOLU, KC_MUTE,
                     _______, _______, _______,      _______,  _______, _______
 )
 };
+
+ uint32_t layer_state_set_user(uint32_t state) {
+     return update_tri_layer_state(state, _RAISE, _LOWER, _ADJUST);
+ }
